45177bd9531ed39223bc2fdd4e60cacb07050500
loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2024-10-16 09:43:43 +0200

    Jag vet inte vad mer jag kan ändra med mina kunskaper som jag har just nu.

--- kort.py
+++ kort.py
@@ -1,28 +1,30 @@
 import random
-player1_hp = 10
-player2_hp = 10
+player1_hp = 25
+player2_hp = 25
 
 while player1_hp > 0 and player2_hp > 0:
 
     player1_roll = random.randint(1, 6)
     player2_roll = random.randint(1, 6)
     
-    print(f"<-----------Player one strikes with: {player1_roll} Damage!----------->")
-    print(f"<-----------Player two strikes with: {player2_roll} Damage!----------->")
+    print(f"<-----------Player one strikes with: {player1_roll} Damage!------------>")
+    print(f"<-----------Player two strikes with: {player2_roll} Damage!------------>")
     
     if player1_roll > player2_roll:
         
         damage = player1_roll - player2_roll
         player2_hp -= damage
-        print(f"<-Player one deals {damage} damage. Player two has {player2_hp} health left.->")
+        print(f"<-Player one deals {damage} damage. Player two has {player2_hp} health left->")
     elif player2_roll > player1_roll:
         
         damage = player2_roll - player1_roll
         player1_hp -= damage
-        print(f"<-Player two deals {damage} damage. Player one has {player1_hp} health left.->")
+        print(f"<-Player two deals {damage} damage. Player one has {player1_hp} health left->")
     else:
-       print("<--------Neither does damage, both dodge the attacks.--------->")
+       print("<--------Neither does damage, both dodge the attacks.---------->")
 if player1_hp <= 0:
-    print("<-------------Player two Killed player one!------------->")
+    print("<--------------Player two Killed player one!-------------->")
 elif player2_hp <= 0:
-    print("<-------------Player one Killed player two!------------->")
+    print("<--------------Player one Killed player two!-------------->")
+
+    
